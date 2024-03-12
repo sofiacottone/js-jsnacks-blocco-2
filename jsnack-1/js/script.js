6e1191dffe443219c4f8aeaa69ab6858dfa859eb
sofiacottone <cottonesofia@outlook.it>
2024-03-12 15:15:40 +0100

    init jsnack-1

---
+++ jsnack-1/js/script.js
@@ -0,0 +1,11 @@
+// L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+
+// chiedo un numero all'utente
+
+// verifico se è pari
+
+// se sì stampo il numero
+
+// se dispari stampo il numero successivo a quello inserito dall'utente

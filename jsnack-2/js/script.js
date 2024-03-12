5733aa9a3be220119f3c7c62bbe2096b511c1bd2
sofiacottone <cottonesofia@outlook.it>
2024-03-12 16:49:25 +0100

    init jsnack-2

---
+++ jsnack-2/js/script.js
@@ -0,0 +1,10 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+// chiedo due parole all'utente
+
+// verifico quale delle due è più lunga
+
+// stampo prima la più corta
+

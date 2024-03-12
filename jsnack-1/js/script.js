9ea6924d44cfbe7168f7155b48b1437fcae99c45
sofiacottone <cottonesofia@outlook.it>
2024-03-12 15:31:38 +0100

    feat: add jsnack-1 logic

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,11 +1,15 @@
 // L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
-
 // chiedo un numero all'utente
+let userNumber = parseInt(prompt('Inserisci un numero'));
 
 // verifico se è pari
+if (userNumber % 2 === 0) {
+    // se sì stampo il numero
+    console.log(userNumber);
 
-// se sì stampo il numero
-
-// se dispari stampo il numero successivo a quello inserito dall'utente
+} else {
+    // se dispari stampo il numero successivo a quello inserito dall'utente
+    console.log(++userNumber);
+}
